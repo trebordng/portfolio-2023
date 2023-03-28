d0cd39aafc45382c70533cf183db6008864e3ca0
trebordng <96870545+trebordng@users.noreply.github.com>
2023-03-29 02:34:23 +1000

    fix directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./Pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
     "./Layout/**/*.{js,ts,jsx,tsx}",
 
