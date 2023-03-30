9f53195dc355776a2bbbdbfc944d679e4bb5790e
trebordng <96870545+trebordng@users.noreply.github.com>
2023-03-30 13:29:00 +1000

    update About Me

--- Layout/Slider/index.tsx
+++ Layout/Slider/index.tsx
@@ -30,6 +30,30 @@ const Slider = ({
       // Fetch from GraphCMS
       const fetchContent = async () =>
         await getContents().then((result: Data) => {
+          console.log(result.projects);
+          const preloadData = () => {
+            for (let i = 0; i < result.contents.length; i++) {
+              var image = new Image();
+              image.onload = () => {
+                if (i === result.contents.length - 1) {
+                  //remove Spin
+                  setTimeout(() => {
+                    setLoading(false);
+                  }, 500);
+                  // for (
+                  //   let iProject = 0;
+                  //   iProject < result.projects.length-1;
+                  //   i++
+                  // ) {
+                  //   var projectImage = new Image();
+                  //   projectImage.onload = () => {};
+                  // }
+                }
+              };
+              image.src = result.contents[i].node.images.url;
+            }
+          };
+          preloadData();
           // Save Data
           setPortrait({
             src: result.contents[0].node.images.url,
@@ -40,11 +64,6 @@ const Slider = ({
             alt: result.contents[1].node.images.alt,
           });
           setProjects(result.projects);
-
-          //remove Spin
-          setTimeout(() => {
-            setLoading(false);
-          }, 500);
         });
       fetchContent();
     }
@@ -63,7 +82,7 @@ const Slider = ({
           className={`${pageColor} absolute top-0 left-0 full-w-h z-0`}
         >
           <m.main
-            initial={{ y: direction==="100%"?"40px":"-40px", opacity: 0 }}
+            initial={{ y: direction === "100%" ? "40px" : "-40px", opacity: 0 }}
             animate={{ y: "0", opacity: 1 }}
             transition={{ delay: 0.5, duration: 0.3, ease: "easeOut" }}
             exit={{ opacity: 1 }}
