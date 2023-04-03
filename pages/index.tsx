9548cc9f5830a48326a6e5f9a4d91de15a1b58b1
trebordng <96870545+trebordng@users.noreply.github.com>
2023-04-03 20:00:03 +1000

    update CV

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Page } from "@/Context/CanvasContext";
 import Slider from "@/Layout/Slider";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
@@ -23,9 +24,11 @@ const Home = () => {
             className="w-192 h-192 sm:w-256 sm:h-256 md:w-320 md:h-320 lg:w-416 lg:h-416 object-cover rounded-full bg-purple"
           />
         </figure>
-        <button className="button md:text-med border-black rounded text-black hover:text-purple hover:border-purple font-bold">
-          My Resumé
-        </button>
+        <a href="./RT Resume.pdf" target="_blank" rel="noopener">
+          <button className="button md:text-med border-black rounded text-black hover:text-purple hover:border-purple font-bold">
+            My Resumé
+          </button>
+        </a>
       </section>
     </Slider>
   );
